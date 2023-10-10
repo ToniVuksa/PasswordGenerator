6b7f52eca2db9120c81bf3e62c3daef3d8e335a1
Toni Vukša <tonivuksa1886@gmail.com>
2023-10-10 17:38:15 +0200

    Commit inital files

---
+++ script.js
@@ -0,0 +1,115 @@
+let characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let firstPass = document.getElementById("passOne");
+let secondPass = document.getElementById("passTwo");
+let passLength = 15;
+
+function randomCharacter() {
+  let randomChar = Math.floor(Math.random() * characters.length);
+  return characters[randomChar];
+}
+
+function randomPassword() {
+  let randomPassword = "";
+  for (let i = 0; i < passLength; i++) {
+    randomPassword += randomCharacter();
+  }
+  return randomPassword;
+}
+
+function passGenerator() {
+  firstPass.textContent = randomPassword();
+  secondPass.textContent = randomPassword();
+}
